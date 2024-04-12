723bdf84b9d8bd737e4bab3f9316287408e2c45d
= <=>
2024-04-12 23:48:14 +0500

    ignore

--- config/settings.py
+++ config/settings.py
@@ -124,12 +124,12 @@ STATICFILES_DIRS = ( BASE_DIR / 'static', )
 STATIC_ROOT = ( BASE_DIR / 'staticfiles' )
 
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+# STORAGES = {
+#     # ...
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
 
 
 
